941ff342cda3ad5b93ccb6f8ee9be5471d862011
udeshika-sewwandi <udeshika.sewwandi@cognite.com>
2022-07-18 11:52:45 +0530

    Expense manager comment.

--- src/main/java/com/sew/expense/expensemanager/controller/ExpenseController.java
+++ src/main/java/com/sew/expense/expensemanager/controller/ExpenseController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExpenseController {
 
+  /**
+   * Returns the sum of the given integers.
+   * @param a first int
+   * @param b second int
+   * @return Returns the sum of the given integers.
+   */
   @GetMapping("/expense/{a}/{b}")
   public int totalExpense(@PathVariable int a, @PathVariable int b) {
     return a + b;
